18d234902bee4dd3b9200e02f7b9a512a84afb53
Chizh13 <antonchizhevich48@fmail.com>
2020-03-06 18:33:43 +0300

    fix: task performance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ 	var x = str.length + 1;
+    while(str = str.replace(/\<\>|\[\]|\{\}|\(\)|\|\||\{\}|[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/,'')){
+        if(str.length == x && x != 0) return false;
+        if(x == 0) return true;
+        x = str.length;
+    }
+    return true;
 }
+
